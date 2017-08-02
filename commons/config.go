13d92abc8590646d0f3ddcf5aa3a54c206abf419
Ronald Jaimes <ronald.jaimes@intraway.com>
2017-08-02 10:53:20 -0500

    upload commons modules

---
+++ commons/config.go
@@ -0,0 +1,106 @@
+package snmp_subagent
+
+import (
+	"errors"
+	"fmt"
+	"io/ioutil"
+
+	yaml "gopkg.in/yaml.v2"
+)
+
+type Config struct {
+	AgentXProtocol  string                       `yaml:"agentx_protocol"`
+	AgentXAddress   string                       `yaml:"agentx_address"`
+	DiscoverTimeout uint32                       `yaml:"discover_timeout"`
+	GetTimeout      uint32                       `yaml:"get_timeout"`
+	EndpointAddress string                       `yaml:"endpoint_address"`
+	DbFile          string                       `yaml:"db_file"`
+	Logging         map[string]map[string]string `yaml:"logging",flow`
+}
+
+func DefaultConfig() Config {
+	c := Config{
+		AgentXProtocol:  "tcp",
+		AgentXAddress:   "localhost:705",
+		DiscoverTimeout: 500,
+		GetTimeout:      300,
+		EndpointAddress: "localhost:8080",
+		DbFile:          "data/applications.db",
+	}
+	return c
+}
+
+func LoadConfig(path string) (Config, error) {
+	yamlstr, err := ioutil.ReadFile(path)
+	// Initialize with default values
+	yamlparsed := DefaultConfig()
+	if err != nil {
+		return yamlparsed, err
+	}
+
+	err = yaml.Unmarshal(yamlstr, &yamlparsed)
+	if err != nil {
+		return yamlparsed, err
+	}
+
+	err = validateLogging(yamlparsed)
+	if err != nil {
+		return yamlparsed, err
+	}
+
+	return yamlparsed, nil
+}
+
+func validateLogging(config Config) error {
+	for log_appender, log_appender_info := range config.Logging {
+		switch log_appender {
+		case "stdout":
+
+		case "file":
+			if _, ok := log_appender_info["dir"]; !ok {
+				return fmt.Errorf("Missing 'dir' in appender %v", log_appender)
+			}
+		default:
+			return fmt.Errorf("Unsupported log appender '%v'", log_appender)
+		}
+
+		err := validateLogLevel(log_appender_info)
+		if err != nil {
+			return err
+		}
+
+		if format, ok := log_appender_info["format"]; ok {
+			err = validateLogFormat(format)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func validateLogLevel(log_info map[string]string) error {
+	level, ok := log_info["level"]
+	if !ok {
+		return errors.New("'level' not present in log appender")
+	}
+
+	if level == "DEBUG" ||
+		level == "INFO" ||
+		level == "NOTICE" ||
+		level == "WARNING" ||
+		level == "ERROR" {
+		return nil
+	} else {
+		return fmt.Errorf("Unknown log level '%v'", level)
+	}
+}
+
+func validateLogFormat(format string) error {
+	if format == "plain" || format == "json" {
+		return nil
+	} else {
+		return fmt.Errorf("Unknown log format '%v'", format)
+	}
+}
---
+++ commons/oid_data.go
@@ -0,0 +1,94 @@
+package snmp_subagent
+
+import (
+	"fmt"
+
+	"github.com/gdey/jsonpath"
+	"github.com/posteo/go-agentx/pdu"
+	"github.com/posteo/go-agentx/value"
+)
+
+type OIDData struct {
+	Oid        value.OID
+	Type       pdu.VariableType
+	Url        string
+	Jsonpath   string
+	Applicator jsonpath.Applicator
+}
+
+func NewOIDData(oid string, oid_type string, url string, jpath string) (*OIDData, error) {
+	real_oid, err := ParseOID(oid)
+	if err != nil {
+		return nil, err
+	}
+
+	real_type, err := string2VariableType(oid_type)
+	if err != nil {
+		return nil, err
+	}
+
+	applicator, err := jsonpath.Parse(jpath)
+	if err != nil {
+		return nil, err
+	}
+
+	oid_data := OIDData{
+		Oid:        real_oid,
+		Type:       real_type,
+		Url:        url,
+		Jsonpath:   jpath,
+		Applicator: applicator,
+	}
+
+	return &oid_data, nil
+}
+
+func string2VariableType(s string) (pdu.VariableType, error) {
+	switch s {
+	case "Integer":
+		return pdu.VariableTypeInteger, nil
+	case "OctetString":
+		return pdu.VariableTypeOctetString, nil
+	case "ObjectIdentifier":
+		return pdu.VariableTypeObjectIdentifier, nil
+	case "IpAddress":
+		return pdu.VariableTypeIPAddress, nil
+	case "Counter32":
+		return pdu.VariableTypeCounter32, nil
+	case "Gauge32":
+		return pdu.VariableTypeGauge32, nil
+	case "TimeTicks":
+		return pdu.VariableTypeTimeTicks, nil
+	case "Opaque":
+		return pdu.VariableTypeOpaque, nil
+	case "Counter64":
+		return pdu.VariableTypeCounter64, nil
+	}
+
+	return pdu.VariableTypeNoSuchObject, fmt.Errorf("%v is not  a valid pdu type", s)
+}
+
+func variableType2String(t pdu.VariableType) (string, error) {
+	switch t {
+	case pdu.VariableTypeInteger:
+		return "Integer", nil
+	case pdu.VariableTypeOctetString:
+		return "OctetString", nil
+	case pdu.VariableTypeObjectIdentifier:
+		return "ObjectIdentifier", nil
+	case pdu.VariableTypeIPAddress:
+		return "IpAddress", nil
+	case pdu.VariableTypeCounter32:
+		return "Counter32", nil
+	case pdu.VariableTypeGauge32:
+		return "Gauge32", nil
+	case pdu.VariableTypeTimeTicks:
+		return "TimeTicks", nil
+	case pdu.VariableTypeOpaque:
+		return "Opaque", nil
+	case pdu.VariableTypeCounter64:
+		return "Counter64", nil
+	}
+
+	return "", fmt.Errorf("%v is not  a valid type", t)
+}
