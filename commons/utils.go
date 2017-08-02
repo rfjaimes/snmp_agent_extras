bd92038d230ad924d22bc732a0d99eafb9e70e50
Ronald Jaimes <ronald.jaimes@intraway.com>
2017-08-02 11:09:31 -0500

    add utils

---
+++ commons/utils.go
@@ -0,0 +1,22 @@
+package snmp_subagent
+
+import "github.com/posteo/go-agentx/value"
+
+// Taken from "github.com/gin-gonic/gin"
+func filterFlags(content string) string {
+	for i, char := range content {
+		if char == ' ' || char == ';' {
+			return content[:i]
+		}
+	}
+	return content
+}
+
+// Wrapper de value.ParseOID que no acepta que
+// el oid arranque con "."
+func ParseOID(oid string) (value.OID, error) {
+	if len(oid) > 0 && oid[0] == '.' {
+		return value.ParseOID(oid[1:])
+	}
+	return value.ParseOID(oid)
+}
---
+++ commons/utils_test.go
@@ -0,0 +1,36 @@
+package snmp_subagent
+
+import (
+	"testing"
+
+	"github.com/posteo/go-agentx/value"
+)
+
+func TestParseOID(t *testing.T) {
+	test_data := []struct {
+		oid            string
+		expected       value.OID
+		error_expected bool
+	}{
+		{"1.3.6", value.MustParseOID("1.3.6"), false},
+		{".1.3.6", value.MustParseOID("1.3.6"), false},
+		{".100.3.6", value.MustParseOID("100.3.6"), false},
+		{".", nil, true},
+		{"1.", nil, true},
+	}
+
+	for _, test_row := range test_data {
+		oid, err := ParseOID(test_row.oid)
+		if test_row.error_expected && err == nil {
+			t.Errorf("Error was expected while parsing '%v' but got nil", test_row.oid)
+		} else if !test_row.error_expected {
+			if err != nil {
+				t.Errorf("Error was not expected while parsing '%v' but got %v", test_row.oid, err)
+			} else {
+				if oid.String() != test_row.expected.String() {
+					t.Errorf("Error parsing oid. Expected: '%v'\nGot %v", test_row.expected, oid)
+				}
+			}
+		}
+	}
+}
