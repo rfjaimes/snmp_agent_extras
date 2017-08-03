0513332ef10a698eda118f700b4ea14f32827f51
Ronald Jaimes <ronald.jaimes@intraway.com>
2017-08-03 12:07:42 -0500

    add php-fpm base scripts

---
+++ commons/subagent.go
@@ -0,0 +1,51 @@
+package commons
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	errgo "gopkg.in/errgo.v1"
+
+	agentx "github.com/posteo/go-agentx"
+	"github.com/posteo/go-agentx/value"
+	"github.com/rfjaimes/snmp_agent_extras/commons"
+	"github.com/rfjaimes/snmp_agent_extras/monitors/cpnr/stats"
+)
+
+var client Client
+var session *Session
+
+
+func StartSubAgent(config Config) {
+
+	client := &agentx.Client{
+		Net:               config.AgentXProtocol,
+		Address:           config.AgentXAddress,
+		Timeout:           1 * time.Minute,
+		ReconnectInterval: 1 * time.Second,
+	}
+
+	if err := client.Open(); err != nil {
+		log.Fatalf(errgo.Details(err))
+	}
+
+	session, err := client.Session()
+	if err != nil {
+		log.Fatalf(errgo.Details(err))
+	}
+}
+
+func RegisterSubAgent(base_oid string, stats_handler *StatsSNMPHandler)
+
+	session.Handler = stats_handler
+
+	if err := session.Register(127, value.MustParseOID(base_oid)); err != nil {
+		log.Fatalf(errgo.Details(err))
+	}
+
+	log.Info(base_oid, "successfully registered")
+
+}
---
+++ monitors/php-fpm/php-fpm.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"flag"
+	"os"
+	"os/signal"
+	"syscall"
+
+	"github.com/rfjaimes/snmp_agent_extras/commons"
+	"github.com/rfjaimes/snmp_agent_extras/monitors/cpnr/stats"
+)
+
+var log = commons.GetLogger()
+
+func main() {
+
+	commons.SetBasicLogger()
+	log.Info("Starting CPRN snmp subagent")
+
+	config_flag := flag.String("config", "../../conf/config.yaml.in", "Path to config file")
+	flag.Parse()
+
+	config, err := commons.LoadConfig(*config_flag)
+	if err != nil {
+		log.Fatalf("Error loading config file: %v", err)
+	}
+
+	commons.ResetLogging(config)
+
+	commons.StartSubAgent(config)
+
+	sm := stats.NewStatsManager()
+	sm.Run(60)
+
+	base_oid := "1.3.6.1.4.1.25934.128.1.11.1.1"
+
+	stats_handler := stats.NewStatsSNMPHandler(sm, base_oid)
+
+	commons.RegisterSubAgent(base_oid, stats_handler)
+
+	sigs := make(chan os.Signal, 1)
+	signal.Notify(sigs, syscall.SIGINT, syscall.SIGTERM)
+	<-sigs
+}
